4fef87d7d8b23d09590459d61991e7f4219f1b60
Emilien Valat <hx21262@bristol.ac.uk>
2023-02-28 09:35:41 +0000

    Walnut downloader, walnut dataloader and paths edit

--- AItomotools/utils/paths.py
+++ AItomotools/utils/paths.py
@@ -1,7 +1,9 @@
-
-## File to contain paths to data created/needed by this toolbox. 
-# If you don't have access to the paths, ask Ander Biguri about it. 
+import pathlib
+## File to contain paths to data created/needed by this toolbox.
+# If you don't have access to the paths, ask Ander Biguri about it.
 
 path_luna="/local/scratch/public/AItomotools/raw/LUNA16"
 path_LIDC_IDRI="/local/scratch/public/ab2860/data/LIDC-IDRI"
 path_projections_luna_2d="/local/scratch/public/AItomotools/processed/LUNA16/2D/"
+
+WALNUT_DATASET_PATH = pathlib.Path("/local/scratch/public/AItomotools/raw/walnuts")
