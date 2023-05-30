8a0f4030d1916a806c7eb97539f65a170564a157
Emilien Valat <ev373@holly-b.damtp.cam.ac.uk>
2023-05-30 16:10:38 +0100

    pre_processing folder and lidc idri dataset with diagnosis and mask

--- AItomotools/data_loaders/LIDC_IDRI.py
+++ AItomotools/data_loaders/LIDC_IDRI.py
@@ -5,10 +5,8 @@ import math
 
 import torch
 import numpy as np
-import pylidc as pl
 import json
 from torch.utils.data import Dataset
-import pydicom as dicom
 
 from backends.odl import ODLBackend
 
@@ -18,66 +16,6 @@ def format_index(index:int) -> str:
         str_index = '0' + str_index
     assert len(str_index) == 4
     return str_index
-
-def get_n_slices(scan:pl.Scan) -> int:
-    n_slices = len(list(pathlib.Path(scan.get_path_to_dicom_files()).glob('*')))
-    print(f'Scan with PID {scan.patient_id} has {n_slices} slices')
-    return n_slices
-
-def get_slice(scan:pl.Scan, slice_index:int) -> np.ndarray:
-    image = scan.load_all_dicom_images()[slice_index]
-    path_to_dicom_files = scan.get_path_to_dicom_files()
-    image = dicom.read_file(pathlib.Path(path_to_dicom_files).joinpath(f'{slice_index}.dcm'))
-    input()
-    return (image.pixel_array * image.RescaleSlope + image.RescaleIntercept).astype(np.int16)
-
-def choose_random_annotation(list_of_nodule_annotations:List[pl.Annotation]) -> pl.Annotation:
-    return random.choice(list_of_nodule_annotations)
-
-def is_nodule_on_slice(annotation:pl.Annotation, slice_index:int, verbose=False) -> Tuple[bool, int]:
-    zmin  = annotation.bbox()[-1].start
-    zmax = annotation.bbox()[-1].stop
-    if zmin <= slice_index <= zmax:
-        if verbose :
-            print(f'The annotated nodule {annotation.id} is present on slice {slice_index}')
-        return True, zmin
-
-    if verbose :
-        print(f'The annotated nodule {annotation.id} is absent on slice {slice_index}')
-    return False, zmin
-
-def get_nodule_boundaries(annotation:pl.Annotation) -> Tuple[int, int, int, int]:
-    return annotation.bbox()[0].start, annotation.bbox()[0].stop, annotation.bbox()[1].start, annotation.bbox()[1].stop
-
-def rescaled_z_index(z, z_min, z_max) -> int:
-    return int(z + (z_max-z_min)/(2*z_min+z_max))
-
-def get_slice_mask(list_of_annotated_nodules:List[pl.Annotation], slice_index:int) -> np.ndarray:
-    background_array = np.ones((512,512), dtype=np.int16)
-    nodule_array = np.zeros((512,512), dtype=np.int16)
-    for annotated_nodule in list_of_annotated_nodules:
-
-        annotation = choose_random_annotation(annotated_nodule)
-        tumor_on_slice, zmin = is_nodule_on_slice(annotation, slice_index)
-
-        if tumor_on_slice:
-
-            ### mask = annotation.boolean_mask()[:,:,slice_index - zmin]
-            ### WARNING indexing shenanigans,
-            ### is there a mismatch between the number of indices when a volume is considered as a volume AND when a single slice is queried?
-            ### EV gets errors once every other sample
-            ### Temporary solution below
-            try:
-                mask = annotation.boolean_mask()[:,:,slice_index - zmin]
-            except IndexError:
-                mask = annotation.boolean_mask()[:,:,slice_index - zmin - 1] ## yuck
-
-            xmin, xmax, ymin, ymax = get_nodule_boundaries(annotation)
-
-            nodule_array[xmin:xmax, ymin:ymax] = np.bitwise_or(nodule_array[xmin:xmax, ymin:ymax], mask)
-            background_array[xmin:xmax, ymin:ymax] = np.bitwise_and(background_array[xmin:xmax, ymin:ymax], np.invert(mask))
-
-    return np.dstack((background_array, nodule_array)).transpose((2,1,0)).transpose((0,2,1))
 
 def load_json(file_path:pathlib.Path):
     if not file_path.is_file():
@@ -93,6 +31,8 @@ class LIDC_IDRI(Dataset):
             ):
 
         self.path_to_processed_dataset = pathlib.Path('/local/scratch/public/AItomotools/processed/LIDC-IDRI')
+        self.patients_masks_dictionary = load_json(self.path_to_processed_dataset.joinpath('patients_masks.json'))
+        self.patients_diagnosis_dictionary = load_json(self.path_to_processed_dataset.joinpath('patients_id_to_diagnosis.json'))
         self.total_patients = 1012
 
         self.pipeline = pipeline
@@ -101,7 +41,6 @@ class LIDC_IDRI(Dataset):
         }
         self.training_proportion = training_proportion
         self.mode = mode
-
 
         self.n_patients_training = math.floor(self.training_proportion*self.total_patients)
         self.n_patients_testing  = math.ceil((1-self.training_proportion)*self.total_patients)
@@ -156,8 +95,30 @@ class LIDC_IDRI(Dataset):
         #### EXPENSIVE ####
         return backend.get_sinogram(self.get_reconstruction_tensor(file_path))
 
-    def get_mask_tensor(self, scan:pl.Scan, slice_index:int) -> torch.Tensor:
-        return torch.from_numpy(get_slice_mask(scan.cluster_annotations(), slice_index)) #type:ignore
+    def get_mask_tensor(self, patient_id:str, slice_index:int) -> torch.Tensor:
+        ## First, assess if the slice has a nodule
+        try:
+            mask = torch.zeros((512,512))
+            for nodule_index, nodule_annotations_list in enumerate(self.patients_masks_dictionary[patient_id][slice_index]):
+                ## If a nodule was not segmented by all the clinicians, the other annotations should not always be seen
+                while len(nodule_annotations_list) < 4:
+                    nodule_annotations_list.append('')
+
+                annotation = random.choice(nodule_annotations_list)
+                if annotation == '':
+                    # Hopefully, that exists the try to return an empty mask
+                    nodule_mask = torch.zeros((512,512))
+                else:
+                    path_to_mask = self.path_to_processed_dataset.joinpath(f'mask_{slice_index}_nodule_{nodule_index}_annotation_{annotation}.npy')
+                    nodule_mask = torch.from_numpy(np.load(path_to_mask))
+
+                mask.bitwise_and(nodule_mask)
+
+        except KeyError:
+            mask = torch.zeros((512,512))
+        # byte inversion
+        background = ~mask
+        return torch.stack((background, mask))
 
     def __len__(self):
         return len(self.slice_index_to_patient_id_list)
@@ -171,14 +132,16 @@ class LIDC_IDRI(Dataset):
 
         ### WE NEVER RETURN THE SINOGRAM TO AVOID COMPUTING IT PER SAMPLE ###
         if self.pipeline == "joint" or self.pipeline == "end_to_end" or self.pipeline == "segmentation":
-            scan:pl.Scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == patient_id).first()  #type:ignore
             reconstruction_tensor = self.get_reconstruction_tensor(file_path)
-            mask_tensor = self.get_mask_tensor(scan, slice_index)
+            mask_tensor = self.get_mask_tensor(patient_id, slice_index)
             return reconstruction_tensor, mask_tensor
 
         elif self.pipeline == "reconstruction":
             reconstruction_tensor = self.get_reconstruction_tensor(file_path)
             return reconstruction_tensor
+
+        elif self.pipeline == "diagnostic":
+            return self.patients_diagnosis_dictionary[patient_id]
 
         else:
             raise NotImplementedError
