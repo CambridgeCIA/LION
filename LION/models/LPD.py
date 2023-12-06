0db962d411c80599cdf858c0019bd4bfc5fb34e6
Christina Runkel <cr661@cam.ac.uk>
2023-12-06 16:30:27 +0000

    Fixed step size bug for LPD

--- LION/models/LPD.py
+++ LION/models/LPD.py
@@ -123,6 +123,7 @@ class LPD(LIONmodel.LIONmodel):
 
         # Define step size
         if self.model_parameters.step_size is None:
+            print("Step size is None, computing it with power method")
             # compute step size
             self.model_parameters.step_size = 1 / power_method(self.op)
 
@@ -179,7 +180,7 @@ class LPD(LIONmodel.LIONmodel):
         LPD_params.data_channels = [7, 32, 32, 5]
         LPD_params.reg_channels = [6, 32, 32, 5]
         LPD_params.learned_step = False
-        LPD_params.step_size = 1
+        LPD_params.step_size = None
         LPD_params.step_positive = False
         LPD_params.mode = "ct"
 
