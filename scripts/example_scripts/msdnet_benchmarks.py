6375a89e1efef3ffe0d35c8cc7ff05318e16c53c
ObiWanWheeler <charliestinhorto@gmail.com>
2024-07-12 10:02:33 +0100

    move msdnet_benchmarks to example_scripts

--- scripts/example_scripts/msdnet_benchmarks.py
+++ scripts/example_scripts/msdnet_benchmarks.py
@@ -5,12 +5,10 @@
 import time
 import numpy as np
 import torch
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 import pathlib
-from LION.experiments import ct_benchmarking_experiments
 from LION.models.CNNs.MSDNets.FBPMS_D import FBPMSD_Net, OGFBPMSD_Net
-from LION.models.CNNs.MSDNets.MS_D import MS_D
 from LION.models.CNNs.MSDNets.MS_D2 import MSD_Params
 from LION.utils.parameter import LIONParameter
 import LION.experiments.ct_experiments as ct_experiments
@@ -18,7 +16,7 @@ import LION.experiments.ct_experiments as ct_experiments
 
 #%%
 # % Chose device:
-device = torch.device("cuda:1")
+device = torch.device("cuda:3")
 torch.cuda.set_device(device)
 # Define your data paths
 savefolder = pathlib.Path("/store/DAMTP/cs2186/trained_models/clinical_dose/")
@@ -26,10 +24,10 @@ savefolder = pathlib.Path("/store/DAMTP/cs2186/trained_models/clinical_dose/")
 
 #
 #%% Define experiment
-experiments = [ct_benchmarking_experiments.FullDataCTRecon(), ct_benchmarking_experiments.LimitedAngle150CTRecon(), ct_benchmarking_experiments.LimitedAngle120CTRecon(), ct_benchmarking_experiments.LimitedAngle90CTRecon(), ct_benchmarking_experiments.LimitedAngle60CTRecon(), ct_benchmarking_experiments.SparseAngle720CTRecon(), ct_benchmarking_experiments.SparseAngle360CTRecon(), ct_benchmarking_experiments.SparseAngle180CTRecon(), ct_benchmarking_experiments.SparseAngle120CTRecon(), ct_benchmarking_experiments.SparseAngle90CTRecon(), ct_benchmarking_experiments.SparseAngle60CTRecon(), ct_benchmarking_experiments.LowDoseCTRecon(), ct_benchmarking_experiments.BeamHardeningCTRecon()]
+experiments = [ct_experiments.clinicalCTRecon(), ct_experiments.ExtremeLowDoseCTRecon(), ct_experiments.LimitedAngleCTRecon(), ct_experiments.SparseAngleCTRecon()]
 
 for experiment in experiments:
-    experiment_str = str(type(experiment)).split("ct_benchmarking_experiments.")[1][:-2]
+    experiment_str = str(type(experiment)).split("experiments.")[1][:-2]
     #%% Dataset
     lidc_dataset = experiment.get_training_dataset()
     lidc_dataset_val = experiment.get_validation_dataset()
@@ -101,10 +99,10 @@ for experiment in experiments:
         for idx, (sinogram, target_reconstruction) in enumerate(tqdm(lidc_dataloader)):   
             sinogram = sinogram.to(device)
             target_reconstruction = target_reconstruction.to(device)
-            with torch.autocast("cuda"):
-                reconstruction = model(sinogram)
-                loss = loss_fcn(reconstruction, target_reconstruction)
-                loss = loss / train_param.accumulation_steps
+            
+            reconstruction = model(sinogram)
+            loss = loss_fcn(reconstruction, target_reconstruction)
+            loss = loss / train_param.accumulation_steps
 
             scaler.scale(loss).backward()
 
@@ -124,6 +122,8 @@ for experiment in experiments:
         with torch.no_grad():
             print("Validating...")
             for sinogram, target_reconstruction in tqdm(lidc_validation):
+                sinogram = sinogram.to(device)
+                target_reconstruction.to(device)
                 reconstruction = model(sinogram)
                 loss = loss_fcn(target_reconstruction, reconstruction.to(device))
                 valid_loss += loss.item()
@@ -134,8 +134,8 @@ for experiment in experiments:
         
         total_validation_time += time.time() - start_time
 
-    with open(f"{experiment_str}_benchmarking.txt", 'w') as f:
-        f.write("Model took {total_train_time/train_param.epochs}s /epoch to train on average\n")
+    with open(f"{experiment_str}_msd2_benchmarking.txt", 'w') as f:
+        f.write(f"Model has {count_parameters(model)} trainable parameters")
         f.write(f"Model took {total_train_time/train_param.epochs}s /epoch to train on average\n")
         f.write(f"Model achieved minimum training loss of {min(total_loss)}\n")
         f.write(f"Model took {total_validation_time/train_param.epochs}s /epoch to validate on average\n")
