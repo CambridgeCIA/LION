b798a38a4dc836427e9008e32146b8ea95a9ed91
hyt35 <hyt35@cam.ac.uk>
2024-07-16 16:57:25 +0100

    add forward to LIONreconstructor

--- LION/reconstructors/reconstructor.py
+++ LION/reconstructors/reconstructor.py
@@ -8,6 +8,7 @@
 
 # Lionmodels
 from LION.models.LIONmodel import LIONmodel
+from LION.models.LIONmodelSubclasses import LIONmodelSino, LIONmodelPhantom
 from LION.experiments.ct_experiments import Experiment
 
 import numpy as np
@@ -18,30 +19,54 @@ from torch.utils.data import DataLoader, Dataset
 # imports related to class organization
 from abc import ABC, abstractmethod, ABCMeta
 
-class LIONreconstructor(ABC):
+class LIONreconstructor(nn.Module):
     def __init__(
             self, 
             model, 
-            data) -> None:
+            data = None) -> None:
         
         assert isinstance(model, LIONmodel), "model must be a LIONmodel"
-        assert isinstance(data, Experiment) or isinstance(data, Dataset) or torch.is_tensor(data), "data must be Experiment, Dataset or batched input"
-        if torch.is_tensor(data):
-            assert len(data.shape) == 3 or len(data.shape) == 4, f"input Tensor must have shape (C,W,H) or (C,D,H,W), currently {data.shape}"
         super().__init__()
         
         self.model = model
         self.model.eval()
 
-
+        if data is not None:
+            assert isinstance(data, Experiment) or isinstance(data, Dataset) or torch.is_tensor(data), "data must be Experiment, Dataset or batched input"
+            if torch.is_tensor(data):
+                assert len(data.shape) == 3 or len(data.shape) == 4, f"input Tensor must have shape (C,W,H) or (C,D,H,W), currently {data.shape}"
+        
         self.dataloader = data # Contains parameters of images to be reconstructed.
-    
-    def __call__(self, *args):
-        return self.forward(args)
 
-    @abstractmethod
     def forward(self, *args):
-        pass
+        if args:
+            data = args[0]
+            assert isinstance(data, Experiment) or isinstance(data, Dataset) or torch.is_tensor(data), "data must be Experiment, Dataset or batched input"
+        elif self.dataloader:
+            data = self.dataloader
+        else:
+            raise RuntimeError("Either pass data when constructing LIONreconstructor, or pass data into the forward operator.")
+
+        if isinstance(data, Experiment):
+            data_iterator = None # TODO get iterator
+        elif isinstance(data, Dataset):
+            data_iterator = None # TODO get iterator
+        elif torch.is_tensor(data):
+            data_iterator = [data]
+        else:
+            raise NotImplementedError(f"Reconstruction is not supported for data of type {type(self.data)}")
+        
+        if isinstance(self.model, LIONmodelSino):
+            pass
+        elif isinstance(self.model, LIONmodelPhantom):
+            pass
+        else:
+            raise NotImplementedError(f"Reconstruction is not supported for model of type {type(self.model)}")
+
+
+        # TODO iterate
+
+
 
 # Postprocessing eg FBPConvNet: takes sinogram and performs FDK reconstruction
 # iterative unrolled: also takes sinogram
