13f9dba14df64196ce48974542955608c5268e08
ObiWanWheeler <charliestinhorto@gmail.com>
2024-08-07 14:36:18 +0100

    change AR cite to original paper

--- LION/optimizers/ARsolver.py
+++ LION/optimizers/ARsolver.py
@@ -255,25 +255,21 @@ class ARSolver(LIONsolver):
     def mini_batch_step(self, sino_batch, target_batch) -> float:
         return self.wgan_loss(sino_batch, target_batch)
 
-    # Change this to original paper
     @staticmethod
     def cite(cite_format="MLA"):
         if cite_format == "MLA":
-            print("Mukherjee, Subhadip, et al.")
-            print('"Data-Driven Convex Regularizers for Inverse Problems."')
-            print(
-                "ICASSP 2024-2024 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP). IEEE, 2024"
-            )
+            print("Lunz, Sebastian, Ozan Öktem, and Carola-Bibiane Schönlieb")
+            print('"Adversarial regularizers in inverse problems."')
+            print("neural information processing systems 31 (2018).")
             print("arXiv:2008.02839 (2020).")
         elif cite_format == "bib":
             string = """
-            @inproceedings{mukherjee2024data,
-            title={Data-Driven Convex Regularizers for Inverse Problems},
-            author={Mukherjee, S and Dittmer, S and Shumaylov, Z and Lunz, S and {\"O}ktem, O and Sch{\"o}nlieb, C-B},
-            booktitle={ICASSP 2024-2024 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)},
-            pages={13386--13390},
-            year={2024},
-            organization={IEEE}
+            @article{lunz2018adversarial,
+            title={Adversarial regularizers in inverse problems},
+            author={Lunz, Sebastian and {\"O}ktem, Ozan and Sch{\"o}nlieb, Carola-Bibiane},
+            journal={Advances in neural information processing systems},
+            volume={31},
+            year={2018}
             }
             """
             print(string)
