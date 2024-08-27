f7472d79c3ecd56bf97f496c9465d2855b74ed93
ObiWanWheeler <charliestinhorto@gmail.com>
2024-08-27 16:39:32 +0100

    SURE modifications

--- LION/losses/SURE.py
+++ LION/losses/SURE.py
@@ -11,27 +11,47 @@ class SURE(nn.Module):
         self.noise_std = noise_std
         self.mse = nn.MSELoss()
 
-    def _monte_carlo_divergence(self, f, y: torch.Tensor):
+    def _monte_carlo_divergence(self, model, y: torch.Tensor):
         # y is B, C, W, H
         B, C, W, H = y.shape
         N = C * W * H
         b = torch.normal(0.0, 1.0, (B, N)).to(y.device)
+        y = y.reshape((B, N))
+
+        def f(x):
+            return model(x.reshape((B, C, W, H))).reshape((B, N))
 
         if self.epsilon is None:
             epsilon = SURE.default_epsilon(y)
         else:
             epsilon = self.epsilon
 
-        # TODO: Find a better way to do this
-        diff = f((y.reshape((B, N)) + epsilon * b).reshape((B, C, W, H))) - f(y)
-        return (1 / epsilon) * (1 / N) * (torch.sum(b * (diff).reshape((B, N)), dim=1))
+        diff = f(y + epsilon * b) - f(y)
+        return torch.sum((1 / (N * epsilon)) * torch.sum(b * diff))
 
     def forward(self, model, noisy):
+        # print("MSE: ", self.mse(noisy, model(noisy)))
+        # # print("Noise_var: ", self.noise_std**2)
+        # # print("Divergence term", 2 * (self.noise_std**2) * self._monte_carlo_divergence(model, noisy))
+        # print("SURE loss ", torch.mean(
+        #     self.mse(noisy, model(noisy))
+        #     - self.noise_std**2
+        #     + 2 * (self.noise_std**2) * self._monte_carlo_divergence(model, noisy)
+        # ))
+        # if torch.mean(
+        #     self.mse(noisy, model(noisy))
+        #     - self.noise_std**2
+        #     + 2 * (self.noise_std**2) * self._monte_carlo_divergence(model, noisy)
+        # ) < 0:
+        #     print("NEGATIVE SURE LOSS")
+        #     quit()
         # mean loss over batch
-        return torch.mean(
-            self.mse(noisy, model(noisy))
-            - self.noise_std**2
-            + 2 * (self.noise_std**2) * self._monte_carlo_divergence(model, noisy)
+        return torch.abs(
+            torch.mean(
+                self.mse(noisy, model(noisy))
+                - self.noise_std**2
+                + 2 * (self.noise_std**2) * self._monte_carlo_divergence(model, noisy)
+            )
         )
 
     @staticmethod
--- scripts/example_scripts/SURE.py
+++ scripts/example_scripts/SURE.py
@@ -1,15 +1,41 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from tqdm import tqdm
 import pathlib
 from LION.CTtools.ct_utils import make_operator
 from LION.classical_algorithms.fdk import fdk
 from LION.losses.SURE import SURE
 from LION.models.iterative_unrolled.ItNet import UNet
-from LION.models.post_processing.FBPConvNet import FBPConvNet
 from LION.utils.parameter import LIONParameter
 import LION.experiments.ct_experiments as ct_experiments
+from skimage.metrics import (
+    structural_similarity as ssim,
+    peak_signal_noise_ratio as psnr,
+)
+
+
+def my_ssim(x: torch.Tensor, y: torch.Tensor):
+    x = x.detach().cpu().numpy().squeeze()
+    y = y.detach().cpu().numpy().squeeze()
+    vals = []
+    if x.shape[0] == 1:
+        return ssim(x, y, data_range=y.max() - y.min())
+    for i in range(x.shape[0]):
+        vals.append(ssim(x[i], y[i], data_range=y[i].max() - y[i].min()))
+    return np.array(vals).mean()
+
+
+def my_psnr(x: torch.Tensor, y: torch.Tensor):
+    x = x.cpu().detach().numpy().squeeze()
+    y = y.cpu().detach().numpy().squeeze()
+    vals = []
+    if x.shape[0] == 1:
+        return psnr(x, y, data_range=y.max() - y.min())
+    for i in range(x.shape[0]):
+        vals.append(psnr(x[i], y[i], data_range=y[i].max() - y[i].min()))
+    return np.array(vals).mean()
 
 
 #%%
@@ -26,11 +52,17 @@ experiment = ct_experiments.clinicalCTRecon()
 op = make_operator(experiment.geo)
 
 lidc_dataset = experiment.get_training_dataset()
-lidc_dataset_val = experiment.get_validation_dataset()
+lidc_dataset = Subset(lidc_dataset, range(200))
 
-batch_size = 3
+lidc_dataset_val = experiment.get_validation_dataset()
+lidc_dataset_val = Subset(lidc_dataset_val, range(50))
+
+lidc_dataset_test = experiment.get_testing_dataset()
+
+batch_size = 2
 lidc_dataloader = DataLoader(lidc_dataset, batch_size, shuffle=True)
-lidc_validation = DataLoader(lidc_dataset_val, batch_size, shuffle=True)
+lidc_validation = DataLoader(lidc_dataset_val, 1, shuffle=True)
+lidc_testing = DataLoader(lidc_dataset_test, 1, shuffle=True)
 
 model = UNet().to(device)
 
@@ -43,6 +75,7 @@ sample_noisy_recon = fdk(sample_sino, op)
 std_estimate = torch.std(sample_gt - sample_noisy_recon)
 
 loss_fcn = SURE(std_estimate.item())
+mse = torch.nn.MSELoss()
 train_param.optimiser = "adam"
 
 # optimizer
@@ -68,9 +101,9 @@ start_epoch = 0
 #    print("final model exists! You already reached final iter")
 #    exit()
 
-model, optimiser, start_epoch, total_loss, _ = UNet.load_checkpoint_if_exists(
-    checkpoint_fname, model, optimiser, total_loss
-)
+# model, optimiser, start_epoch, total_loss, _ = UNet.load_checkpoint_if_exists(
+#     checkpoint_fname, model, optimiser, total_loss
+# # )
 
 total_loss = np.resize(total_loss, train_param.epochs)
 print(f"Starting iteration at epoch {start_epoch}")
@@ -78,7 +111,6 @@ print(f"Starting iteration at epoch {start_epoch}")
 #%% train
 for epoch in range(start_epoch, train_param.epochs):
     train_loss = 0.0
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimiser, steps)
     for sinogram, _ in tqdm(lidc_dataloader):
         optimiser.zero_grad()
         bad_recon = fdk(sinogram, op)
@@ -89,18 +121,17 @@ for epoch in range(start_epoch, train_param.epochs):
         train_loss += loss.item()
 
         optimiser.step()
-        scheduler.step()
     total_loss[epoch] = train_loss
     # Validation
     valid_loss = 0.0
     model.eval()
     for sinogram, target_reconstruction in tqdm(lidc_validation):
-        reconstruction = model(sinogram)
-        loss = loss_fcn(target_reconstruction, reconstruction)
+        reconstruction = model(fdk(sinogram, op))
+        loss = mse(target_reconstruction, reconstruction)
         valid_loss += loss.item()
 
     print(
-        f"Epoch {epoch+1} \t\t Training Loss: {train_loss / len(lidc_dataloader)} \t\t Validation Loss: {valid_loss / len(lidc_validation)}"
+        f"Epoch {epoch+1} \t\t Training Loss: {train_loss / (batch_size * len(lidc_dataloader))} \t\t Validation Loss: {valid_loss / (batch_size * len(lidc_validation))}"
     )
 
     if min_valid_loss > valid_loss:
@@ -128,6 +159,60 @@ for epoch in range(start_epoch, train_param.epochs):
             dataset=experiment.param,
         )
 
+ssims = []
+psnrs = []
+printed = 0
+with torch.no_grad():
+    for sinogram, gt in tqdm(lidc_testing):
+
+        sinogram = sinogram.to(device)
+        gt = gt.to(device)
+        bad_recon = fdk(sinogram, op)
+        bad_mse = mse(bad_recon, gt)
+        bad_ssim = my_ssim(bad_recon, gt)
+        bad_psnr = my_psnr(bad_recon, gt)
+
+        output = model(bad_recon)
+        cur_mse = mse(output, gt)
+        cur_ssim = my_ssim(output, gt)
+        cur_psnr = my_psnr(output, gt)
+
+        ssims = np.append(ssims, cur_ssim)
+        if np.isfinite(cur_psnr):
+            psnrs = np.append(psnrs, cur_psnr)
+
+        if printed < 3:
+            plt.figure()
+            plt.subplot(131)
+            plt.imshow(gt[0].detach().cpu().numpy().T)
+            plt.clim(torch.min(gt[0]).item(), torch.max(gt[0]).item())
+            plt.gca().set_title("Ground Truth")
+            plt.axis("off")
+            plt.subplot(132)
+            # should cap max / min of plots to actual max / min of gt
+            plt.imshow(bad_recon[0].detach().cpu().numpy().T)
+            plt.clim(torch.min(gt[0]).item(), torch.max(gt[0]).item())
+            plt.gca().set_title("FBP")
+            plt.text(0, 650, f"{bad_ssim:.2f}\n{bad_psnr:.2f}")
+            plt.axis("off")
+            plt.subplot(133)
+            plt.imshow(output[0].detach().cpu().numpy().T)
+            plt.clim(torch.min(gt[0]).item(), torch.max(gt[0]).item())
+            plt.gca().set_title("SURE")
+            plt.text(0, 650, f"{cur_ssim:.2f}\n{cur_psnr:.2f}")
+            plt.axis("off")
+            # reconstruct filepath with suffix i
+            plt.savefig(f"SURE{printed}.png", dpi=700)
+            plt.close()
+
+            printed += 1
+
+    print(
+        f"Average ssim: {np.mean(ssims)}, Max ssim: {np.max(ssims)}, Min ssim: {np.min(ssims)}"
+    )
+    print(
+        f"Average psnr: {np.mean(psnrs)}, Max psnr: {np.max(psnrs)}, Min psnr: {np.min(psnrs)}"
+    )
 
 model.save(
     final_result_fname,
