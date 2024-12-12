74a507acab899e0ba32e2a7ddc26769711c4276c
Christina Runkel <cr661@cam.ac.uk>
2024-12-12 16:41:19 +0000

    Chnaged paths

--- LION/utils/paths.py
+++ LION/utils/paths.py
@@ -4,13 +4,13 @@ import pathlib
 # If you don't have access to the paths and you are part of the research group, ask Ander Biguri about it.
 # Otherwise, feel free to change those to your paths.
 
-LION_DATA_PATH = pathlib.Path("/store/LION/datasets/")
+LION_DATA_PATH = pathlib.Path("/home/cr661/rds/hpc-work/store/LION/data")
 LUNA_DATASET_PATH = LION_DATA_PATH.joinpath("raw/LUNA16")
 WALNUT_DATASET_PATH = LION_DATA_PATH.joinpath("raw/walnuts")
-LIDC_IDRI_PATH = LION_DATA_PATH.joinpath("raw/LIDC-IDRI")
+LIDC_IDRI_PATH = LION_DATA_PATH.joinpath("LIDC-IDRI")
 DETECT_PATH = LION_DATA_PATH.joinpath("raw/2detect")
 
 ## Data ready for training use
 LUNA_PROCESSED_DATASET_PATH = LION_DATA_PATH.joinpath("processed/LUNA16")
-LIDC_IDRI_PROCESSED_DATASET_PATH = LION_DATA_PATH.joinpath("processed/LIDC-IDRI")
+LIDC_IDRI_PROCESSED_DATASET_PATH = LION_DATA_PATH.joinpath("LIDC-IDRI")
 DETECT_PROCESSED_DATASET_PATH = LION_DATA_PATH.joinpath("processed/2detect")
