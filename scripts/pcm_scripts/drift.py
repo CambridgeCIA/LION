2fc5bdc52b677321701a596573ba3ac48791ead5
ttv22 <ttv22@cam.ac.uk>
2026-01-20 07:21:08 +0100

    Visualise drift

--- scripts/pcm_scripts/drift.py
+++ scripts/pcm_scripts/drift.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 
 def plot_drift():
     data_dir = Path("data/photocurrent_data")
-    file_path = data_dir / "Si_2_256_measurement_data.npy"
+    # data_name = "Si_2_256"
+    data_name = "Si_256"
+    file_path = data_dir / f"{data_name}_measurement_data.npy"
     data = np.load(file_path)  # shape (N, 2): first column is index of Hadamard pattern, second column is measured current in Amperes
     # Add every two consecutive measurements
     summed_data = data[::2, 1] + data[1::2, 1]  # shape (N/2,)
@@ -16,12 +18,11 @@ def plot_drift():
     plt.plot(np.arange(num_measurements), inverted_summed_data)
     plt.xlabel("Measurement index (sum of positive and negative patterns)")
     plt.ylabel("Current (A)")
-    plt.title("Drift of measured current over time")
+    plt.title(f"Drift of measured current for {data_name} over time")
     plt.grid(True)
     plt.tight_layout()
     # plt.show()
-    plt.savefig(data_dir / "drift_plot.png")
-
+    plt.savefig(data_dir / f"{data_name}_drift_plot.png")
 
 if __name__ == "__main__":
     plot_drift()
