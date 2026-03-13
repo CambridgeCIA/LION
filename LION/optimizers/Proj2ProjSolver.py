7603faeb1806ffe32c5cc6123a1e95818eed5bbc
Alvaro Exposito Mtz <alvaro.exposito@alumni.mondragon.edu>
2026-03-13 15:22:03 +0000

    Reformat and CT_Utils new add noise function

--- LION/optimizers/Proj2ProjSolver.py
+++ LION/optimizers/Proj2ProjSolver.py
@@ -14,6 +14,7 @@ import tomosipo as ts
 import LION.CTtools.ct_utils as ct_utils
 from tomosipo.torch_support import to_autograd
 
+
 class Proj2ProjSolver(LIONsolver):
     def __init__(
         self,
@@ -37,56 +38,62 @@ class Proj2ProjSolver(LIONsolver):
         )
 
         self.operator = ct_utils.make_operator(self.geometry)
-        self.model.geometry = self.geometry  
+        self.model.geometry = self.geometry
         self.model.operator = self.operator
         self.projector = to_autograd(self.operator, num_extra_dims=1)
         self.recon_fn = self.solver_params.recon_fn
-        self.global_step=0
+        self.global_step = 0
 
     def get_mask(self, shape, step):
         # shape: (B, C, H, W)
         mask = torch.ones(shape, device=self.device)
-        grid = self.solver_params.grid_size 
-        
+        grid = self.solver_params.grid_size
+
         for b in range(shape[0]):
-            idx = (step+b) % (grid * grid)
+            idx = (step + b) % (grid * grid)
             r = idx // grid
             c = idx % grid
 
             mask[b, :, r::grid, c::grid] = 0
         return mask
-    
+
     def fill_mean(self, sinos, mask):
-        kernel = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=torch.float32, device=self.device) / 4.0
-        
+        kernel = (
+            torch.tensor(
+                [[0, 1, 0], [1, 0, 1], [0, 1, 0]],
+                dtype=torch.float32,
+                device=self.device,
+            )
+            / 4.0
+        )
+
         kernel = kernel.view(1, 1, 3, 3)
         local_mean_sino = F.conv2d(sinos, kernel, padding=1)
-        
+
         filled_sinos = (sinos * mask) + (local_mean_sino * (1 - mask))
         return filled_sinos
 
     @staticmethod
     def default_parameters() -> LIONParameter:
         params = LIONParameter()
-        params.grid_size = 4 
+        params.grid_size = 4
         params.recon_fn = fdk
         return params
-    
-    def mini_batch_step(self, sinos, targets):
-        mask=self.get_mask(sinos.shape, self.global_step)
-        self.global_step += sinos.shape[0]
-        input_sino=self.fill_mean(sinos,mask)
-        
-        input_recon=self.recon_fn(input_sino,self.model.operator)
-        output_recon = self.model(input_recon)
-        output_sino=self.projector(output_recon)
 
-        output_sino_mask=output_sino*(1-mask)
-        target_sino=sinos*(1-mask)
+    def mini_batch_step(self, sinos, targets):
+        mask = self.get_mask(sinos.shape, self.global_step)
+        self.global_step += sinos.shape[0]
+        input_sino = self.fill_mean(sinos, mask)
+
+        input_recon = self.recon_fn(input_sino, self.model.operator)
+        output_recon = self.model(input_recon)
+        output_sino = self.projector(output_recon)
+
+        output_sino_mask = output_sino * (1 - mask)
+        target_sino = sinos * (1 - mask)
 
         batch_loss = ((output_sino_mask - target_sino) ** 2).mean()
         return batch_loss
-
 
     # No validation in Proj2Proj
     def validate(self):
